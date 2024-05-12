a4eecb313290695664911bab9f78b964cc31fa19
Vordgi <sasha2822222@gmail.com>
2024-05-12 22:52:43 +0400

    na-81 nimpl news april-may

---
+++ src/pages/blog/_meta.ts
@@ -0,0 +1,21 @@
+const config = {
+  "index": {
+    "type": "page",
+    "title": "Blog",
+    "theme": {
+      "sidebar": false,
+      "typesetting": "article",
+      "pagination": false
+    }
+  },
+  "*": {
+    "type": "page",
+    "theme": {
+      "sidebar": false,
+      "typesetting": "article",
+      "pagination": false
+    }
+  },
+}
+
+export default config;
